7374a64185c87c84a1a252b821d951b33ac7e8c3
jayatikant <jkant@xebia.com>
2016-09-26 16:40:19 +0530

    Refactoring test case

--- addition/src/test/java/com/tdd/example/addition/CalculateTest.java
+++ addition/src/test/java/com/tdd/example/addition/CalculateTest.java
@@ -6,14 +6,10 @@ import org.junit.Test;
 
 public class CalculateTest {
 
-	@Test
-	public void canCreateCalculator	() {
-		Calculator calculator = new Calculator();
-	}
-	
+	private Calculator calculator = new Calculator();
+
 	@Test
 	public void canCreateAddMethod(){
-		Calculator calculator = new Calculator();
 		int sum = calculator.add("1");
 		assertEquals(1, sum);
 	}
