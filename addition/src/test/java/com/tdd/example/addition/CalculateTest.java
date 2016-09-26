87c6c0b051174e97cebcdffe9d0ac09d0e42ea8b
jayatikant <jkant@xebia.com>
2016-09-26 16:44:50 +0530

    Adding empty and single number functionality

--- addition/src/test/java/com/tdd/example/addition/CalculateTest.java
+++ addition/src/test/java/com/tdd/example/addition/CalculateTest.java
@@ -14,4 +14,15 @@ public class CalculateTest {
 		assertEquals(1, sum);
 	}
 
+	@Test
+	public void returnZeroForEmptyString(){
+		int sum = calculator.add("");
+		assertEquals(0, sum);
+	}
+	
+	@Test
+	public void returnSumForASingleNumber(){
+		int sum = calculator.add("3");
+		assertEquals(3, sum);
+	}
 }
