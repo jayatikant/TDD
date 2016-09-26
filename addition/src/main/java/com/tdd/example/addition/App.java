b7ca690cd003b87811ee939068679377fe155844
jayatikant <jkant@xebia.com>
2016-09-26 16:08:48 +0530

    Created Project for Addition example

---
+++ addition/src/main/java/com/tdd/example/addition/App.java
@@ -0,0 +1,13 @@
+package com.tdd.example.addition;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ addition/src/test/java/com/tdd/example/addition/CalculateTest.java
@@ -0,0 +1,14 @@
+package com.tdd.example.addition;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
