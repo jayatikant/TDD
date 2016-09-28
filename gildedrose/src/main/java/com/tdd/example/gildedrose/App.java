c9ed411a6eeb2eba330ba08e6ecead99b949e2c9
jayatikant <jkant@xebia.com>
2016-09-28 15:59:54 +0530

    Creating new project

---
+++ gildedrose/src/main/java/com/tdd/example/gildedrose/App.java
@@ -0,0 +1,13 @@
+package com.tdd.example.gildedrose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
