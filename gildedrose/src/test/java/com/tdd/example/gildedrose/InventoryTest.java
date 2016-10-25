1faf55181003152a4b86b4491451ed9274e21d95
jayatikant <jkant@xebia.com>
2016-10-25 11:48:11 +0530

    Renaming exercise

--- gildedrose/src/test/java/com/tdd/example/gildedrose/InventoryTest.java
+++ gildedrose/src/test/java/com/tdd/example/gildedrose/InventoryTest.java
@@ -3,14 +3,14 @@ package com.tdd.example.gildedrose;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class GildedRoseTest {
+public class InventoryTest {
 	
 	@Test
-	public void updateQuality_QualityIsNeverNegative() {
+	public void updateStock_QualityIsNeverNegative() {
 		
 		Item[] items = new Item[]{new Item("Testitem", 30, 0)};
-		GildedRose gildedRose = new GildedRose(items );
-		gildedRose.updateQuality();
+		Inventory gildedRose = new Inventory(items );
+		gildedRose.updateStock();
 		Assert.assertEquals(0, gildedRose.items[0].quality);
 	}
 }
